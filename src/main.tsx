18d18b67795189ef19eb85bad9609c697da834c0
Tracy Falba <tfalba@mac.com>
2026-03-03 05:17:55 -0500

    adding analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/next";
 
 
 createRoot(document.getElementById("root")!).render(
@@ -10,5 +11,6 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <Analytics />
   </StrictMode>
 );
